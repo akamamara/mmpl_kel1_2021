a1e15978aac0854fdded2cd029aac8c0852c4e77
akamamara <aldiokta10@hotmail.co.id>
2021-11-04 05:57:31 +0700

    Add new colors

--- mppl-simjababeka-fe/styles/global_mui.js
+++ mppl-simjababeka-fe/styles/global_mui.js
@@ -4,11 +4,15 @@ const defaultTheme = createTheme({
 	palette: {
 		primary: {
 			main: "#D6544B",
+			light: "#EA8983",
 			contrastText: "#fff",
 		},
 		secondary: {
 			main: "#D8B47F",
 			contrastText: "#323232",
+		},
+		text: {
+			primary: "#323232",
 		},
 	},
 });
