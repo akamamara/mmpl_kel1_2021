c9670b036f43c3b03f3a0a3287199de68d14b544
Aldi Oktaviana Hidayat <aldiokta10@hotmail.co.id>
2021-11-21 08:39:03 +0700

    Make carousel dynamic import

--- mppl-simjababeka-fe/sections/beranda/Carousel.js
+++ mppl-simjababeka-fe/sections/beranda/Carousel.js
@@ -1,6 +1,13 @@
+import dynamic from "next/dynamic";
+import * as React from "react";
+
 import { Box } from "@mui/system";
 import { ThemeProvider } from "@mui/material";
-import Carousel from "react-material-ui-carousel";
+// import Carousel from "react-material-ui-carousel";
+
+const Carousel = dynamic(() => import("react-material-ui-carousel"), {
+	ssr: false,
+});
 
 import CarouselItems from "@/components/display/CarouselItems";
 import defaultTheme from "@/styles/global_mui";
@@ -8,23 +15,23 @@ import CarouselList from "@/utils/list/CarouselDummy";
 
 function CarouselSection() {
 	return (
-		<Box mb={8}>
-			{/* <Carousel> */}
-			<ThemeProvider theme={defaultTheme}>
-				{CarouselList.map((item, index) => (
-					<CarouselItems
-						key={index}
-						imageSrc={item.imageSrc}
-						title={item.title}
-						description={item.description}
-						action={item.actionTitle.length ? true : false}
-						actionTitle={item.actionTitle}
-						actionLink={item.actionLink}
-					/>
-				))}
-			</ThemeProvider>
-			{/* </Carousel> */}
-		</Box>
+		<ThemeProvider theme={defaultTheme}>
+			<Box mb={8}>
+				<Carousel>
+					{CarouselList.map((item, index) => (
+						<CarouselItems
+							key={index}
+							imageSrc={item.imageSrc}
+							title={item.title}
+							description={item.description}
+							action={item.actionTitle.length ? true : false}
+							actionTitle={item.actionTitle}
+							actionLink={item.actionLink}
+						/>
+					))}
+				</Carousel>
+			</Box>
+		</ThemeProvider>
 	);
 }
 
