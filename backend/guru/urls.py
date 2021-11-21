aa5e6a3f2946f30f9f49ed7dd1b91cf55128f065
romstormy27 <fahruryanfuhrer27@gmail.com>
2021-11-21 20:04:32 +0700

    ubah endpoint login

--- backend/guru/urls.py
+++ backend/guru/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
 from .views import GuruList, RegisterView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 
 urlpatterns = [
     path('', GuruList.as_view()),
     path('register/', RegisterView.as_view()),
-    path('login/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
