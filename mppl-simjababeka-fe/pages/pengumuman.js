7012f247d8024bc0ab537486f04cf4303afeeab7
Aldi Oktaviana Hidayat <aldiokta10@hotmail.co.id>
2021-11-21 11:08:48 +0700

    Add new pages

--- mppl-simjababeka-fe/pages/pengumuman.js
+++ mppl-simjababeka-fe/pages/pengumuman.js
@@ -1,19 +1,39 @@
-import AdminLayout from '../layouts/AdminLayout'
+import { Box, Grid } from "@mui/material";
 
-const PengumumanPage = () => {
-  return (
-    <>
-        
-    </>
-  )
+import NonLoginLayout from "@/layouts/NonLoginLayout";
+import { CardContent, CardImages, Card } from "@/components/surfaces/Card";
+import { Title } from "@/components/typography/Heading";
+import Body1 from "@/components/typography/Body";
+
+function PengumumanPage() {
+	return (
+		<Box mt={7}>
+			<Title>Berita</Title>
+			<Grid container spacing={3}>
+				{Array.from({ length: 16 }, (_) => (
+					<Grid item xs={3}>
+						<Card>
+							<CardImages />
+							<CardContent>
+								<Body1>
+									<b>Judul</b>
+								</Body1>
+								<Body1 sx={{ marginTop: 1, opacity: 0.6 }}>
+									Lorem ipsum dolor sit amet, consectetur , mmolestie eleifend
+									vitae ac enim. Integer vel pulvinar mauris, nec porttitor
+									justo.das...
+								</Body1>
+							</CardContent>
+						</Card>
+					</Grid>
+				))}
+			</Grid>
+		</Box>
+	);
 }
 
 PengumumanPage.getLayout = (page) => {
-  return (
-    <AdminLayout>
-        {page}
-    </AdminLayout>
-  )
-}
+	return <NonLoginLayout>{page}</NonLoginLayout>;
+};
 
 export default PengumumanPage;
