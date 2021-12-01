e41c36420f06146cfc709767e43d4fbc16af19c1
Aldi Oktaviana Hidayat <aldiokta10@hotmail.co.id>
2021-12-01 10:45:20 +0700

    Add stepper + tambah guru form

--- mppl-simjababeka-fe/pages/admin/tambahguru.js
+++ mppl-simjababeka-fe/pages/admin/tambahguru.js
@@ -2,95 +2,94 @@ import * as React from "react";
 import AdminLayout from "@/layouts/AdminLayout";
 import { Heading3 } from "@/components/typography/Heading";
 import {
-  AgamaList,
-  GolonganDarahList,
-  JenisKelaminList,
+	AgamaList,
+	GolonganDarahList,
+	JenisKelaminList,
 } from "@/utils/list/SelectList";
 import FormTambahGuru from "@/sections/form/FormTambahGuru";
 
+import Container from "@/components/surfaces/Container";
+import Stepper from "@/components/navigation/Stepper";
+
+import { registerValue } from "@/utils/list/BiodataList";
+import { registerGuru, putProfileID } from "@/utils/api/user";
+
 const TambahGuruPage = () => {
-  const [result, setResult] = React.useState([]);
-  const [jenisKelamin, setJenisKelamin] = React.useState("");
+	const [resultRegister, setResultRegister] = React.useState([]);
+	const [resultProfile, setResultProfile] = React.useState([]);
+	const [currentId, setCurrentId] = React.useState(null);
+	const [activeStep, setActiveStep] = React.useState(0);
 
-  const handleChange = (event, title) => {
-    if (title === "Jenis Kelamin") {
-      setJenisKelamin(event.target.value);
-      setResult({ ...result, ["Jenis Kelamin"]: event.target.value });
-    }
-  };
+	const registerHandler = {
+		handleInput: (e) => {
+			setResultRegister({ ...resultRegister, [e.target.name]: e.target.value });
+		},
 
-  const checkValue = (title) => {
-    if (title === "Jenis Kelamin") {
-      return jenisKelamin;
-    }
-  };
+		handleSubmit: async () => {
+			console.log("Data Register", resultRegister);
+			registerGuru(1, setCurrentId, resultRegister);
+		},
+	};
 
-  const checkData = (title) => {
-    if (title === "Jenis Kelamin") {
-      return JenisKelaminList;
-    }
-  };
+	const profileHandler = {
+		handleInput: (e) => {
+			setResultProfile({ ...resultProfile, [e.target.name]: e.target.value });
+		},
 
-  const handleCheckbox = (e, value) => {
-    console.log(value);
-    setResult({ ...result, ["Mata Pelajaran yang Diampu"]: value });
-  };
+		handleSubmit: async () => {
+			console.log("Data edit", resultProfile);
+			putProfileID(currentId, "guru", resultProfile);
+		},
+	};
 
-  const handleInput = (e) => {
-    setResult({ ...result, [e.target.name]: e.target.value });
-  };
+	const stepperFunction = {
+		handleBack: () => {
+			setActiveStep((prev) => prev - 1);
+		},
+		handleNext: () => {
+			if (activeStep === 0)
+				registerHandler
+					.handleSubmit()
+					.then(() => setActiveStep((prev) => prev + 1));
+			if (activeStep === stepContent.length - 1)
+				profileHandler
+					.handleSubmit()
+					.then(() => setActiveStep((prev) => prev + 1));
+		},
+	};
 
-  const handleSimpan = () => {
-    // const isEmpty = !Object.values(result).some((x) => x !== null && x !== "");
-    if (
-      result["Nama"] &&
-      result["Email"] &&
-      result["NIP"] &&
-      result["Pendidikan (Gelar)"] &&
-      result["Mata Pelajaran yang Diampu"] &&
-      result["Tempat, Tanggal Lahir"] &&
-      result["No. Telepon"] &&
-      result["Alamat"] &&
-      result["Jenis Kelamin"] &&
-      result["Password"] &&
-      result["Confirm Password"]
-    ) {
-      const simpan = {
-        nama: result["Nama"],
-        email: result["Email"],
-        nip: result["NIP"],
-        pendidikan: result["Pendidikan (Gelar)"],
-        mapelDiampu: result["Mata Pelajaran yang Diampu"],
-        ttl: result["Tempat, Tanggal Lahir"],
-        noTelp: result["No. Telepon"],
-        alamat: result["Alamat"],
-        jk: result["Jenis Kelamin"],
-        password: result["Password"],
-        password2: result["Confirm Password"],
-      };
-      console.log(simpan);
-    } else {
-      console.log("Isi formnya ya");
-    }
-  };
+	const stepContent = [
+		{
+			label: "Register Guru",
+			content: (
+				<FormTambahGuru
+					handleInput={registerHandler.handleInput}
+					list={registerValue}
+				/>
+			),
+		},
+		{
+			label: "Tambah Profil Guru",
+			content: <FormTambahGuru handleInput={profileHandler.handleInput} />,
+		},
+	];
 
-  return (
-    <>
-      <Heading3 sx={{ mb: 1 }}>Tambah Guru Baru</Heading3>
-      <FormTambahGuru
-        handleChange={handleChange}
-        checkValue={checkValue}
-        checkData={checkData}
-        handleCheckbox={handleCheckbox}
-        handleInput={handleInput}
-        handleSimpan={handleSimpan}
-      />
-    </>
-  );
+	return (
+		<>
+			<Heading3 sx={{ mb: 1 }}>Tambah Guru Baru</Heading3>
+			<Container>
+				<Stepper
+					stepperFunction={stepperFunction}
+					activeStep={activeStep}
+					steps={stepContent}
+				/>
+			</Container>
+		</>
+	);
 };
 
 TambahGuruPage.getLayout = (page) => {
-  return <AdminLayout>{page}</AdminLayout>;
+	return <AdminLayout>{page}</AdminLayout>;
 };
 
 export default TambahGuruPage;
