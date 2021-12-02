582b93a44004e68478ed5168183b9196d725d76b
Aldi Oktaviana Hidayat <aldiokta10@hotmail.co.id>
2021-12-02 11:44:47 +0700

    Change new line behavior

--- mppl-simjababeka-fe/pages/informasi.js
+++ mppl-simjababeka-fe/pages/informasi.js
@@ -10,51 +10,61 @@ import defaultTheme from "@/styles/global_mui";
 import Body1 from "@/components/typography/Body";
 
 function HomepageLayouts() {
-  return (
-    <div>
-      <CarouselItems
-        title="Informasi"
-        imageSrc="http://foto2.data.kemdikbud.go.id/getImage/20246387/3.jpg"
-      />
-      <Container>
-        <Grid
-          container
-          columnSpacing={8}
-          direction="row"
-          sx={{ position: "relative", marginTop: defaultTheme.spacing(8) }}
-        >
-          <Grid item sx={{ position: "sticky", top: 110, height: "100%" }}>
-            <Heading3 sx={{ textAlign: "right" }}>Daftar Isi</Heading3>
-            <Box sx={{ textAlign: "right" }}>
-              {InformasiList.map((item) => (
-                <Links key={item.name} href={"#" + item.name.toLowerCase()}>
-                  {item.name}
-                </Links>
-              ))}
-            </Box>
-          </Grid>
-          <Grid item sx={{ width: "82%" }}>
-            {InformasiList.map((item) => (
-              <Box
-                id={item.name.toLowerCase()}
-                key={item.name}
-                sx={{ marginBottom: defaultTheme.spacing(3) }}
-              >
-                <Title smallMargin>{item.name}</Title>
-                <Body1 sx={{ whiteSpace: "pre-line" }}>
-                  {item.description}
-                </Body1>
-              </Box>
-            ))}
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
-  );
+	return (
+		<div>
+			<CarouselItems
+				title="Informasi"
+				imageSrc="http://foto2.data.kemdikbud.go.id/getImage/20246387/3.jpg"
+			/>
+			<Container>
+				<Grid
+					container
+					columnSpacing={8}
+					direction="row"
+					sx={{ position: "relative", marginTop: defaultTheme.spacing(8) }}
+				>
+					<Grid item sx={{ position: "sticky", top: 110, height: "100%" }}>
+						<Heading3 sx={{ textAlign: "right" }}>Daftar Isi</Heading3>
+						<Box sx={{ textAlign: "right" }}>
+							{InformasiList.map((item) => (
+								<Links key={item.name} href={"#" + item.name.toLowerCase()}>
+									{item.name}
+								</Links>
+							))}
+						</Box>
+					</Grid>
+					<Grid item sx={{ width: "82%" }}>
+						{InformasiList.map((item) => (
+							<Box
+								id={item.name.toLowerCase()}
+								key={item.name}
+								sx={{ marginBottom: defaultTheme.spacing(3) }}
+							>
+								<Title smallMargin>{item.name}</Title>
+								{item.description.split("\n").map((it, index) => (
+									<Body1
+										paragraph
+										sx={{
+											whiteSpace: "pre-line",
+											marginBottom: 2,
+											lineHeight: "150%",
+										}}
+										key={index}
+									>
+										{it}
+									</Body1>
+								))}
+							</Box>
+						))}
+					</Grid>
+				</Grid>
+			</Container>
+		</div>
+	);
 }
 
 HomepageLayouts.getLayout = (page) => {
-  return <NonLoginLayout container={false}>{page}</NonLoginLayout>;
+	return <NonLoginLayout container={false}>{page}</NonLoginLayout>;
 };
 
 export default HomepageLayouts;
