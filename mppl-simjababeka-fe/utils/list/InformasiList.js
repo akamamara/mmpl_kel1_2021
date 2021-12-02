d406ba7c39f6a2b29aeedb20bb7e3c1da81d948b
pascalpanatagama <pascal_20@apps.ipb.ac.id>
2021-12-02 11:17:58 +0700

    added informasi

--- mppl-simjababeka-fe/utils/list/InformasiList.js
+++ mppl-simjababeka-fe/utils/list/InformasiList.js
@@ -1,29 +1,29 @@
 const InformasiList = [
-	{
-		name: "Profil",
-		description:
-			"Lorem ipsum dolor sit  amet, consectetur adipiscing elit. Ac hac faucibus eget enim tempor ut ullamcorper molestie ornare. Gravida metus pharetra quam sodales dui aenean consequat gravida. Mattis convallis tellus et orci pharetra, in mauris. Et sit potenti tellus fringilla nec, lorem vitae.",
-	},
-	{
-		name: "Sejarah",
-		description:
-			"Lorem ipsum dolor sit  amet, consectetur adipiscing elit. Ac hac faucibus eget enim tempor ut ullamcorper molestie ornare. Gravida metus pharetra quam sodales dui aenean consequat gravida. Mattis convallis tellus et orci pharetra, in mauris. Et sit potenti tellus fringilla nec, lorem vitae.",
-	},
-	{
-		name: "Visi dan Misi",
-		description:
-			"Lorem ipsum dolor sit  amet, consectetur adipiscing elit. Ac hac faucibus eget enim tempor ut ullamcorper molestie ornare. Gravida metus pharetra quam sodales dui aenean consequat gravida. Mattis convallis tellus et orci pharetra, in mauris. Et sit potenti tellus fringilla nec, lorem vitae.",
-	},
-	{
-		name: "Jurusan",
-		description:
-			"Lorem ipsum dolor sit  amet, consectetur adipiscing elit. Ac hac faucibus eget enim tempor ut ullamcorper molestie ornare. Gravida metus pharetra quam sodales dui aenean consequat gravida. Mattis convallis tellus et orci pharetra, in mauris. Et sit potenti tellus fringilla nec, lorem vitae.",
-	},
-	{
-		name: "Sarana Infrastruktur",
-		description:
-			"Lorem ipsum dolor sit  amet, consectetur adipiscing elit. Ac hac faucibus eget enim tempor ut ullamcorper molestie ornare. Gravida metus pharetra quam sodales dui aenean consequat gravida. Mattis convallis tellus et orci pharetra, in mauris. Et sit potenti tellus fringilla nec, lorem vitae.",
-	},
+  {
+    name: "Profil",
+    description:
+      "SMK Industri Jababeka adalah Sekolah Menengah Kejuruan (SMK) Swasta yang berlokasi di Propinsi Jawa Barat Kabupaten Kab. Bekasi dengan alamat Jln Pangeran Jayakarta.",
+  },
+  {
+    name: "Sejarah",
+    description:
+      "SMK Industri Jababeka didirikan berdasarkan Surat Keputusan (SK) Kepala Dinas Pendidikan Kabupaten Bekasi No. 421/276 - Disdik pada tanggal 24 Oktober 2002. Surat Keputusan tersebut berisi tentang pemberian izin operasional kepada Yayasan Pendidikan Sumber Daya Manusia (YPPSDM) di Kabupaten Bekasi untuk Sekolah Menengah Kejuruan (SMK) Industri Jababeka bidang keahlian Teknologi dan Industri. SMK Industri Jababeka mempunyai Sertifikat Nomor Pokok Sekolah Nasional (NPSN) yaitu 20246387. SMK Industri Jababeka pada tahun 2019 mengalami transformasi manajemen dan di akuisisi oleh Bapak Armin Sidiq S.Kom, M. Si. Oleh karena itu, alamat baru SMK Industri Jababeka terletak di Jl. Mangkunegara No. 11 Kp. Rawa Lintah, RT 02/02, Desa Mekarmukti, Kecamatan Cikarang Utara, Kabupaten Bekasi.",
+  },
+  {
+    name: "Visi dan Misi",
+    description:
+      "Visi:\nMewujudkan Sekolah Menengah Kejuruan Swasta sebagai lembaga pendidikan kejuruan yang menghasilkan Sumber Daya Manusia (SDM) yang profesional dan kompeten di bidangnya serta berjiwa wirausaha untuk memenuhi tuntutan Industri dan dunia kerja dalam era globalisasi yang memiliki ketakwaan kepada Tuhan Yang Maha Esa\n\nMisi:\n1. Meningkatkan kualitas iman, takwa kepada tuhan yang maha esa melalui kegiatan ibadah dan pengamalan dalam kehidupan sehari-hari sehingga terwujud peserta didik yang berbudi pekerti luhur, dan berakhlak mulia\n2. Meningkatkan Kwalitas kompetensi Tenaga Pendidik danTenaga Kependidikan agar dapat memberikan pelayanan yang lebih kompetitif.\n3. Mengembangkan Pendidikan dan Pelatihan sesuai dengan kebutuhan Dunia Usaha dan Dunia Industri\n4. Mengembangkan sarana prasarana sesuai kebutuhan masing-masing kompetensi keahlian dalam menghadapi perubahan dan persaingan global\n5. Mewujudkan unit produksi sebagai wahana pelatihan berbasis produksi dan kewirausahaan\n6. Membangun kerjasama dengan industri dan dunia kerja, dalam praktik kerja lapangan, dan pemasaran lulusan\n7. Menyiapkan lulusan agar mampu memiliki karier dalam bidangnya, komukatif, mandiri, berwirausaha, dan mampu melanjutkan studi ke jenjang yang lebih tinggi",
+  },
+  {
+    name: "Jurusan",
+    description:
+      "Bidang program dan keahlian SMK Industri Jababeka:\n1. Teknik Kendaraan Ringan\n2. Teknik Komputer dan Jaringan",
+  },
+  {
+    name: "Sarana Infrastruktur",
+    description:
+      "Setiap satuan pendidikan wajib memiliki sarana yang meliputi perabot, peralatan pendidikan, media pendidikan, buku dan sumber belajar lainnya, bahan habis pakai, serta perlengkapan lain yang diperlukan untuk menunjang proses pembelajaran yang teratur dan berkelanjutan.\nMengacu Peraturan Menteri Pendidikan Nasional Republik Indonesia no 40 tahun 2008 tentang Standar Sarana dan Prasarana.\nSetiap satuan pendidikan wajib memiliki prasarana yang meliputi lahan, ruang kelas, ruang pimpinan satuan pendidikan, ruang pendidik, ruang tata usaha, ruang perpustakaan, ruang laboratorium, ruang kerja, ruang unit produksi, ruang kantin, instalasi daya dan jasa, tempat berolahraga, tempat beribadah, tempat bermain, tempat berkreasi, dan ruang/tempat lain yang diperlukan untuk menunjang proses pembelajaran yang teratur dan berkelanjutan.",
+  },
 ];
 
 export default InformasiList;
